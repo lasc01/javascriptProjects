6ddb1b97ba36c82bc78a91b8107a0ee1c4b163f2
MAC <mac@DJ-LASCS-MAC.local>
2023-01-06 17:19:18 +0100

    initial commit for the fibonachi task

---
+++ fibonachi.js
@@ -0,0 +1,27 @@
+// Fibonachi is a task that adds the last 2 values and push the result as the next value
+
+function fibonachi(n){
+
+	var fibGen = [];
+
+	var num1 = [0];
+
+	var num2 = [0, 1];
+
+	if (n === 1){
+		fibGen = num1;
+	} else if (n === 2){
+		fibGen = num2;
+	} else{
+		fibGen = num2;
+
+		for (i = 2; i < n; i++){
+		fibGen.push(fibGen[fibGen.length - 2] + fibGen[fibGen.length - 1])
+		}
+	}
+	
+	console.log(fibGen);
+
+}
+
+fibonachi(12);
